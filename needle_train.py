1f6ffc92d5e39e3385e51823b90e7edbad6dcdbc
ryan831026 <ryan831026@gmail.com>
2022-12-11 15:09:18 -0500

    disable random in validation

--- needle_train.py
+++ needle_train.py
@@ -37,16 +37,30 @@ def train_net(
 
     # 1. Create dataset
     try:
-        dataset = CarvanaDataset(dir_img, dir_mask, img_scale)
+        dataset = CarvanaDataset(dir_img, dir_mask, img_scale, False)
+        dataset_val = CarvanaDataset(dir_img, dir_mask, img_scale, True)
     except (AssertionError, RuntimeError):
-        dataset = BasicDataset(dir_img, dir_mask, img_scale)
+        dataset = BasicDataset(dir_img, dir_mask, img_scale, False)
+        dataset_val = CarvanaDataset(dir_img, dir_mask, img_scale, True)
+
+    # # 2. Split into train / validation partitions
+    # n_val = int(len(dataset) * val_percent)
+    # n_train = len(dataset) - n_val
+    # train_set, val_set = random_split(
+    #     dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0)
+    # )
 
     # 2. Split into train / validation partitions
     n_val = int(len(dataset) * val_percent)
     n_train = len(dataset) - n_val
-    train_set, val_set = random_split(
-        dataset, [n_train, n_val], generator=torch.Generator().manual_seed(0)
+    train_set_idx, val_set_idx = random_split(
+        range(len(dataset)),
+        [n_train, n_val],
+        generator=torch.Generator().manual_seed(0),
     )
+
+    train_set = torch.utils.data.Subset(dataset, train_set_idx)
+    val_set = torch.utils.data.Subset(dataset_val, val_set_idx)
 
     # 3. Create data loaders
     loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
--- utils/data_loading_needle.py
+++ utils/data_loading_needle.py
@@ -11,39 +11,60 @@ import albumentations as A
 
 
 class BasicDataset(Dataset):
-    def __init__(self, images_dir: str, masks_dir: str, scale: float = 1.0, mask_suffix: str = ''):
+    def __init__(
+        self,
+        images_dir: str,
+        masks_dir: str,
+        scale: float = 1.0,
+        mask_suffix: str = "",
+        if_val=False,
+    ):
         self.images_dir = Path(images_dir)
         self.masks_dir = Path(masks_dir)
-        assert 0 < scale <= 1, 'Scale must be between 0 and 1'
+        assert 0 < scale <= 1, "Scale must be between 0 and 1"
         self.scale = scale
         self.mask_suffix = mask_suffix
+        self.if_val = if_val
 
-        self.ids = [splitext(file)[0] for file in listdir(
-            images_dir) if not file.startswith('.')]
+        self.ids = [
+            splitext(file)[0]
+            for file in listdir(images_dir)
+            if not file.startswith(".")
+        ]
         if not self.ids:
             raise RuntimeError(
-                f'No input file found in {images_dir}, make sure you put your images there')
-        logging.info(f'Creating dataset with {len(self.ids)} examples')
+                f"No input file found in {images_dir}, make sure you put your images there"
+            )
+        logging.info(f"Creating dataset with {len(self.ids)} examples")
 
     def __len__(self):
         return len(self.ids)
 
     @staticmethod
-    def preprocess(pil_img, pil_mask, scale):
+    def preprocess(pil_img, pil_mask, scale, if_val):
         w, h = pil_img.size
         newW, newH = int(scale * w), int(scale * h)
-        assert newW > 0 and newH > 0, 'Scale is too small, resized images would have no pixel'
+        assert (
+            newW > 0 and newH > 0
+        ), "Scale is too small, resized images would have no pixel"
 
-        if not pil_mask == None:
-            transform = A.Compose([
-                A.Flip(p=0.2),
-                A.CropNonEmptyMaskIfExists(height=700, width=700, p=.2),
-                A.GridDistortion(p=0.2),
-                A.ColorJitter(brightness=0.3, contrast=0.2, saturation=0.1,
-                              hue=0.1, always_apply=False, p=0.2),
-
-                A.Resize(newH, newW, interpolation=2)
-            ])
+        if not pil_mask == None or not if_val:
+            transform = A.Compose(
+                [
+                    A.Flip(p=0.2),
+                    A.CropNonEmptyMaskIfExists(height=700, width=700, p=0.2),
+                    A.GridDistortion(p=0.2),
+                    A.ColorJitter(
+                        brightness=0.3,
+                        contrast=0.2,
+                        saturation=0.1,
+                        hue=0.1,
+                        always_apply=False,
+                        p=0.2,
+                    ),
+                    A.Resize(newH, newW, interpolation=2),
+                ]
+            )
         else:
             transform = A.Resize(newH, newW, interpolation=2)
 
@@ -52,13 +73,14 @@ class BasicDataset(Dataset):
         if not pil_mask == None:
             mask_ndarray = np.asarray(pil_mask)
             transformed = transform(
-                image=img_ndarray, mask=mask_ndarray.astype(np.uint8))
-            img_ndarray = transformed['image'] / 255
-            mask_ndarray = transformed['mask'].astype(bool)
+                image=img_ndarray, mask=mask_ndarray.astype(np.uint8)
+            )
+            img_ndarray = transformed["image"] / 255
+            mask_ndarray = transformed["mask"].astype(bool)
 
         else:
             transformed = transform(image=img_ndarray)
-            img_ndarray = transformed['image'] / 255
+            img_ndarray = transformed["image"] / 255
 
         if img_ndarray.ndim == 2:
             img_ndarray = img_ndarray[np.newaxis, ...]
@@ -73,36 +95,41 @@ class BasicDataset(Dataset):
     @staticmethod
     def load(filename):
         ext = splitext(filename)[1]
-        if ext == '.npy':
+        if ext == ".npy":
             return Image.fromarray(np.load(filename))
-        elif ext in ['.pt', '.pth']:
+        elif ext in [".pt", ".pth"]:
             return Image.fromarray(torch.load(filename).numpy())
         else:
             return Image.open(filename)
 
     def __getitem__(self, idx):
         name = self.ids[idx]
-        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + '.*'))
-        img_file = list(self.images_dir.glob(name + '.*'))
+        mask_file = list(self.masks_dir.glob(name + self.mask_suffix + ".*"))
+        img_file = list(self.images_dir.glob(name + ".*"))
 
-        assert len(
-            img_file) == 1, f'Either no image or multiple images found for the ID {name}: {img_file}'
-        assert len(
-            mask_file) == 1, f'Either no mask or multiple masks found for the ID {name}: {mask_file}'
+        assert (
+            len(img_file) == 1
+        ), f"Either no image or multiple images found for the ID {name}: {img_file}"
+        assert (
+            len(mask_file) == 1
+        ), f"Either no mask or multiple masks found for the ID {name}: {mask_file}"
         mask = self.load(mask_file[0])
         img = self.load(img_file[0])
 
-        assert img.size == mask.size, \
-            f'Image and mask {name} should be the same size, but are {img.size} and {mask.size}'
+        assert (
+            img.size == mask.size
+        ), f"Image and mask {name} should be the same size, but are {img.size} and {mask.size}"
 
-        img, mask = self.preprocess(img, mask, self.scale)
+        img, mask = self.preprocess(img, mask, self.scale, self.if_val)
 
         return {
-            'image': torch.as_tensor(img.copy()).float().contiguous(),
-            'mask': torch.as_tensor(mask.copy()).long().contiguous()
+            "image": torch.as_tensor(img.copy()).float().contiguous(),
+            "mask": torch.as_tensor(mask.copy()).long().contiguous(),
         }
 
 
 class CarvanaDataset(BasicDataset):
-    def __init__(self, images_dir, masks_dir, scale=1):
-        super().__init__(images_dir, masks_dir, scale, mask_suffix='_mask')
+    def __init__(self, images_dir, masks_dir, scale=1, if_val=False):
+        super().__init__(
+            images_dir, masks_dir, scale, mask_suffix="_mask", if_val=if_val
+        )
